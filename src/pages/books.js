6944dd9545f7bb687380de542c064f05746e2f93
Prashant <chauhanprashant480@gmail.com>
2024-12-15 23:06:30 +0530

    books card added

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,5 +1,7 @@
 import { gql } from "@apollo/client";
 import createApolloClient from "./apollo_client";
+import Image from "next/image";
+import Link from "next/link";
 
 const GET_BOOKS = gql`
     query Books {
@@ -14,7 +16,50 @@ const GET_BOOKS = gql`
 
 const BooksComponent = (props) => {
     console.log("books props", props);
-    return <h1>Books Page</h1>;
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {props.books.map((book) => (
+                <div
+                    key={book.id}
+                    className="bg-white shadow-md rounded-lg p-4 border border-gray-200"
+                >
+                    {book.cover ? (
+                        <Image
+                            src={book.cover}
+                            alt={book.title}
+                            className="w-20 h-20 rounded-full mx-auto"
+                        />
+                    ) : (
+                        ""
+                    )}
+                    <h3 className="text-lg font-semibold text-start mt-4 capitalize">
+                        {book.title}
+                    </h3>
+                    <p className="text-gray-600 text-start capitalize">
+                        {book.description}
+                    </p>
+                    {/* Action Buttons */}
+                    <div className="mt-4 flex justify-end">
+                        <Link
+                            href={"/books/" + book.id}
+                            className="bg-yellow-500 text-white  px-3 py-1 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                        >
+                            View Details
+                        </Link>
+                        <button className="bg-green-500 text-white px-3  py-1 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400">
+                            Edit
+                        </button>
+                        <button
+                            className="bg-red-500 text-white px-3 py-1  rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+                            // onClick={() => handleDelete(user.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export const getServerSideProps = async (context) => {
