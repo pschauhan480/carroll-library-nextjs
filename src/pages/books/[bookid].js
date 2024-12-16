3047173b74f77d19083fcebf0dd849b07b2ed0d8
Prashant <chauhanprashant480@gmail.com>
2024-12-16 12:55:18 +0530

    book id page added

--- src/pages/books/[bookid].js
+++ src/pages/books/[bookid].js
@@ -1,9 +1,48 @@
-const BookPage = () => {
-    const BookComponent = (props) => {
-        return <h1>Book Page</h1>;
-    };
+import { useState } from "react";
 
-    return <BookComponent />;
+import { fetchBooks } from "../books";
+
+const BookComponent = (props) => {
+    console.log("given props", props);
+    const [book, setBook] = useState(props.book);
+    return (
+        <div className="p-6 max-w-lg mx-auto bg-white rounded shadow-md">
+            <h1 className="text-2xl font-bold mb-4">Book Details</h1>
+            <p>
+                <strong>ID:</strong> {book.id}
+            </p>
+            <p>
+                <strong>Title:</strong> {book.title}
+            </p>
+            <p>
+                <strong>Description:</strong> {book.description}
+            </p>
+            {book.born_date ? (
+                <p>
+                    <strong>Date of Publishing:</strong> {book.published_date}
+                </p>
+            ) : (
+                ""
+            )}
+        </div>
+    );
 };
 
-export default BookPage;
+export const getServerSideProps = async (context) => {
+    const { bookid } = context.params;
+    const books = await fetchBooks({
+        id: bookid,
+    });
+    console.log("fetch book by id", bookid, books);
+    let book = {};
+    if (books.length > 0) {
+        book = books[0];
+    }
+    return {
+        props: {
+            book: book,
+        },
+    };
+};
+
+export default BookComponent;
