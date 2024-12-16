ba9f6f8f4974ebc84fda8b0fd4ea61a49d2a0bbf
Prashant <chauhanprashant480@gmail.com>
2024-12-16 10:40:44 +0530

    authors save details handler and check for authors list logic added

--- src/pages/authors.js
+++ src/pages/authors.js
@@ -3,6 +3,8 @@ import createApolloClient from "./apollo_client";
 import Image from "next/image";
 import Link from "next/link";
 
+import { useState } from "react";
+
 import * as Dialog from "@radix-ui/react-dialog";
 
 const GET_AUTHORS = gql`
@@ -17,6 +19,18 @@ const GET_AUTHORS = gql`
 `;
 
 const AuthorsComponent = (props) => {
+    const [author, setAuthor] = useState({ name: "", biography: "" });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAuthor({ ...author, [name]: value });
+    };
+
+    const saveAuthor = async (event) => {
+        event.preventDefault();
+        console.log("save author details", author);
+    };
+
     console.log("authors props", props);
     return (
         <div className="p-4">
@@ -31,40 +45,46 @@ const AuthorsComponent = (props) => {
                         <Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
                         <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
                             <Dialog.Title className="m-0 text-[17px] font-medium text-mauve12">
-                                Create Book
+                                Add New Author
                             </Dialog.Title>
-                            <Dialog.Description className="mb-5 mt-2.5 text-[15px] leading-normal text-mauve11">
-                                Add new book to the current list
-                            </Dialog.Description>
                             <fieldset className="mb-[15px] flex items-center gap-5">
                                 <label
                                     className="w-[90px] text-right text-[15px] text-violet11"
-                                    htmlFor="title"
+                                    htmlFor="name"
                                 >
-                                    Title
+                                    Full Name
                                 </label>
                                 <input
                                     className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-                                    id="title"
-                                    defaultValue=""
+                                    id="name"
+                                    type="text"
+                                    name="name"
+                                    value={author.name}
+                                    onChange={handleChange}
                                 />
                             </fieldset>
                             <fieldset className="mb-[15px] flex items-center gap-5">
                                 <label
                                     className="w-[90px] text-right text-[15px] text-violet11"
-                                    htmlFor="description"
+                                    htmlFor="biography"
                                 >
-                                    Description
+                                    About
                                 </label>
                                 <input
                                     className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-                                    id="description"
-                                    defaultValue=""
+                                    type="text"
+                                    id="biography"
+                                    name="biography"
+                                    value={author.biography}
+                                    onChange={handleChange}
                                 />
                             </fieldset>
                             <div className="mt-[25px] flex justify-end">
                                 <Dialog.Close asChild>
-                                    <button className="inline-flex h-[35px] items-center justify-center rounded bg-green4 px-[15px] font-medium leading-none text-green11 hover:bg-green5 focus:shadow-[0_0_0_2px] focus:shadow-green7 focus:outline-none">
+                                    <button
+                                        onClick={saveAuthor}
+                                        className="inline-flex h-[35px] items-center justify-center rounded bg-green4 px-[15px] font-medium leading-none text-green11 hover:bg-green5 focus:shadow-[0_0_0_2px] focus:shadow-green7 focus:outline-none"
+                                    >
                                         Save
                                     </button>
                                 </Dialog.Close>
@@ -80,46 +100,50 @@ const AuthorsComponent = (props) => {
                 </div>
             </Dialog.Root>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {props.authors.map((author) => (
-                    <div
-                        key={author.id}
-                        className="bg-white shadow-md rounded-lg p-4 border border-gray-200"
-                    >
-                        {author.avatar ? (
-                            <Image
-                                src={author.avatar}
-                                alt={author.name}
-                                className="w-20 h-20 rounded-full mx-auto"
-                            />
-                        ) : (
-                            ""
-                        )}
-                        <h3 className="text-lg font-semibold text-start mt-4 capitalize">
-                            {author.name}
-                        </h3>
-                        <p className="text-gray-600 text-start capitalize">
-                            {author.biography}
-                        </p>
-                        {/* Action Buttons */}
-                        <div className="mt-4 flex justify-end">
-                            <Link
-                                href={"/authors/" + author.id}
-                                className="bg-yellow-500 text-white  px-3 py-1 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                            >
-                                View Details
-                            </Link>
-                            <button className="bg-green-500 text-white px-3  py-1 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400">
-                                Edit
-                            </button>
-                            <button
-                                className="bg-red-500 text-white px-3 py-1  rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
-                                // onClick={() => handleDelete(user.id)}
-                            >
-                                Delete
-                            </button>
+                {props.author && props.author.length > 0 ? (
+                    props.authors.map((author) => (
+                        <div
+                            key={author.id}
+                            className="bg-white shadow-md rounded-lg p-4 border border-gray-200"
+                        >
+                            {author.avatar ? (
+                                <Image
+                                    src={author.avatar}
+                                    alt={author.name}
+                                    className="w-20 h-20 rounded-full mx-auto"
+                                />
+                            ) : (
+                                ""
+                            )}
+                            <h3 className="text-lg font-semibold text-start mt-4 capitalize">
+                                {author.name}
+                            </h3>
+                            <p className="text-gray-600 text-start capitalize">
+                                {author.biography}
+                            </p>
+                            {/* Action Buttons */}
+                            <div className="mt-4 flex justify-end">
+                                <Link
+                                    href={"/authors/" + author.id}
+                                    className="bg-yellow-500 text-white  px-3 py-1 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                                >
+                                    View Details
+                                </Link>
+                                <button className="bg-green-500 text-white px-3  py-1 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400">
+                                    Edit
+                                </button>
+                                <button
+                                    className="bg-red-500 text-white px-3 py-1  rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                    // onClick={() => handleDelete(user.id)}
+                                >
+                                    Delete
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))
+                ) : (
+                    <p>No Authors found</p>
+                )}
             </div>
         </div>
     );
