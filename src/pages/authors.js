df3842222e6fb489156e6a543147ca8ea603a58c
Prashant <chauhanprashant480@gmail.com>
2024-12-16 11:32:57 +0530

    authors list updated

--- src/pages/authors.js
+++ src/pages/authors.js
@@ -123,7 +123,7 @@ const AuthorsComponent = (props) => {
                 </div>
             </Dialog.Root>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {props.author && props.author.length > 0 ? (
+                {props.authors && props.authors.length > 0 ? (
                     props.authors.map((author) => (
                         <div
                             key={author.id}
